02813e8bc7b98b7a5b1137e04266821f4cdc103a
mutellipiqbal <32098494+mutellipiqbal@users.noreply.github.com>
2017-10-12 22:34:50 +0300

    Update ChickenAndRabit.py

--- practical solutions/ChickenAndRabit.py
+++ practical solutions/ChickenAndRabit.py
@@ -1,3 +1,7 @@
+'''
+The total numbers of  rabits and chickesn are 18 and their foots are 108. Could you find how much rabits and chickens there?
+
+
 for x in range(1,20):
    for y in range (1,20):
       if (x +y ==18) and (2*x + 4*y ==48):
