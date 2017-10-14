7b9e47e5e7622ef4d74d9f367379725136fc8d45
mutellipiqbal <32098494+mutellipiqbal@users.noreply.github.com>
2017-10-14 18:17:16 +0300

    Update type report.py

--- Types/type report.py
+++ Types/type report.py
@@ -1,15 +1,36 @@
-'''
-a = "Mutellip"
-try:
-    tur = int(a)
-    print "basarili"
-except ValueError:
-    print "basarisiz"
-'''
-      
+
 a = raw_input("enter your number or words: ")
+
 try:
+
     int(a)
+
     print "successful"
+
 except ValueError:
+
     print "unsuccessful"
+
+a = 9876
+print type(a)
+
+b = 1000000000000000000
+print type(b)
+
+c = -109
+print type(c)
+
+d = "Mutellip"
+print type(d)
+
+e = {"me":"other side of I", "we":"weakness of you"}
+print type(e)
+
+f = [1,2,3,"other"]
+print type(f)
+
+g = "sen", "ben", "onlar"
+print type(g)
+
+
+    
