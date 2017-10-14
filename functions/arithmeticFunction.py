4b641218070e85147fe51e624c91973429d1dad5
mutellipiqbal <32098494+mutellipiqbal@users.noreply.github.com>
2017-10-14 18:45:29 +0300

    Update arithmeticFunction.py

--- functions/arithmeticFunction.py
+++ functions/arithmeticFunction.py
@@ -1,25 +1,34 @@
-a = int(raw_input("enter firts number: "))
-b = int(raw_input("enter seconf number: "))
-islem = raw_input("enter one of them (*,+,/,-)" )
+
+islem = raw_input("enter one of them (*,+,/,-):" )
+a = input("enter firts number: ")
+b = input("enter seconf number: ")
+
 
 def carpi():
     carpi = a * b
     return carpi
+
 def topla():
     topla = a + b
     return topla
+
 def cikar():
     cikar = a - b
     return cikar
+
 def bolu():
-    bolu = a / b
+    bolu = float(a) / b
     return bolu
 
+
+
 if islem == "+":
-    print topla()
+    print "your result is : ", topla()
+
 if islem == "/":
-    print bolu()
+    print "your result is : ", bolu()
+
 if islem == "*":
-    print carpi()
+    print "your result is : ", carpi()
 if islem == "-":
-    print cikar()
+    print "your result is : ", cikar()
