f2647736a9865974821bf686a4b574b8598f2ab5
mutellipiqbal <32098494+mutellipiqbal@users.noreply.github.com>
2017-10-15 22:54:47 +0300

    Update primeNumber.py

--- practical solutions/primeNumber.py
+++ practical solutions/primeNumber.py
@@ -1,22 +1,17 @@
 def isPrime(i):
     result = True
     k = i-1
+
     while ( k > 1):
         if (i % k == 0):
             result = False
             break
         k = k - 1
-    return result
-
-
-def isPrime2(i):
-
-    for k in range(2, i):
-        if (i % k == 0):
-            
-
+        return result
+    
 
 for t in range (1, 1000):
     if isPrime(t):
         print t, "----", isPrime(t)
+
     
