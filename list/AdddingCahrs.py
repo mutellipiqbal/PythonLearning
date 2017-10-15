d000c8269a65c548eda7e3fb4ff26656097e2638
mutellipiqbal <32098494+mutellipiqbal@users.noreply.github.com>
2017-10-15 21:20:20 +0300

    Update AdddingCahrs.py

--- list/AdddingCahrs.py
+++ list/AdddingCahrs.py
@@ -1,6 +1,4 @@
-
-
-my_list = ['foo', 'fob', 'faz', 'funk']
-string = 'bar'
-my_new_list = [x + string for x in my_list]
-print my_new_list
+myList = ['craft', 'auto', 'wo', 'soft']
+new = "men"
+myNewList = [x + new for x in myList]
+print myNewList
