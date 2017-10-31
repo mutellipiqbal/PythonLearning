732467a6059cb56a742d1d1f7372b85783447371
mutellipiqbal <32098494+mutellipiqbal@users.noreply.github.com>
2017-10-31 19:36:13 +0300

    Add files via upload

---
+++ practical solutions/webbrewser.py
@@ -0,0 +1,36 @@
+
+
+'''
+import time
+time.sleep(10)
+import webbrowser
+webbrowser.open("https://www.youtube.com/watch?v=WAhAjqMeb84")
+'''
+
+
+import time
+import webbrowser
+
+
+totalBreak = 3
+breakCount = 0
+print ("This song is strated on " + time.ctime())
+
+while ( breakCount < totalBreak ):
+
+   webbrowser.open("https://www.youtube.com/watch?v=VHtjz4pucwU")
+   time.sleep(300)
+   breakCount = breakCount + 1
+
+   
+  
+
+
+
+
+
+
+
+
+
+
