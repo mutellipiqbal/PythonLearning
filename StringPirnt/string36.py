667476cca362fae843494a1a7928b819f4dabb67
mutellipiqbal <32098494+mutellipiqbal@users.noreply.github.com>
2017-11-02 12:04:50 +0300

    Add files via upload

---
+++ StringPirnt/string36.py
@@ -0,0 +1,13 @@
+string = "Hello world"
+print ("string1 output is: ", len(string))
+print ("string2 output is: ", string[4])
+print ("string3 output is: ", string[1:])
+print ("string4 output is: ", string[:6])
+print ("string5 output is: ", string[3:6])
+print ("string6 output is: ", string[::2])
+print ("string7 output is: ", string[::-1])
+print ("string8 output is: ", string[:-1])
+print ("string9 output is: ", string*2)
+print ("string10 output is: ", string +", "+"I like you.")
+print ("string11 output is: ", string.split("e"))
+
