fb2ffb3d132ed502c122e96cdbeb6be9662d9155
mutellipiqbal <32098494+mutellipiqbal@users.noreply.github.com>
2017-11-02 19:20:55 +0300

    Add files via upload

---
+++ functions/basicFunction.py
@@ -0,0 +1,12 @@
+def name_write():
+    print "mutellip iqbal"
+name_write()
+
+def surname_write(surname): # we can add attribute to our function
+    print "hello, " + surname
+surname_write("ikbal")
+
+def add_number(number1, number2):
+    return number1 + number2
+x = add_number(23,45)
+print "the sum of two number is :", x
